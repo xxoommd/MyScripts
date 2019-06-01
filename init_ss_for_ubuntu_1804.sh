3084509c465c79609f6ada1f867ae208f53c68ba
gaofei <xxoommd@163.com>
2019-06-01 14:44:01 +0800

    Update

--- init_ss_for_ubuntu_1804.sh
+++ init_ss_for_ubuntu_1804.sh
@@ -14,5 +14,5 @@ curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 sudo usermod -aG docker `whoami`
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo docker run -dt --restart always --name ssserver -p 9527:6443 -p 6500:6500/udp mritd/shadowsocks -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m chacha20 -k fuckyouall --fast-open" -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2"
-sudo docker run -d -p 9528:443 --name=mtproto --ulimit nofile=98304:98304 --restart=always -v proxy-config:/data -e SECRET=abcdef1234567890987654321fedcdba telegrammessenger/proxy
+docker run -dt --restart always --name ssserver -p 9527:6443 -p 6500:6500/udp mritd/shadowsocks -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m chacha20-ietf-poly1305 -k fuckyouall" -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2"
+# sudo docker run -d -p 9528:443 --name=mtproto --ulimit nofile=98304:98304 --restart=always -v proxy-config:/data -e SECRET=abcdef1234567890987654321fedcdba telegrammessenger/proxy
